81ab7795acdadaf2155b15831ebf8208d306b50e
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-02-25 19:42:59 -0300

    atualiza-membros(upd): Atualiza session.cookie para uso também em HTTPs.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,8 +2,7 @@
 
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_strict_mode', 1);
-
-#ini_set('session.cookie_secure', 1); //NOSONAR
+ini_set('session.cookie_secure', 1);
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
