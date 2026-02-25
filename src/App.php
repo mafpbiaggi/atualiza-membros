d63dc7acf7d96e70b23af151315c1d17bbcea54a
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-02-25 20:38:24 -0300

    atualiza-membros(upd): Aplica validação de campos no backend.

--- src/App.php
+++ src/App.php
@@ -12,17 +12,52 @@ function validaCsrf($token) {
     return hash_equals($_SESSION['csrf_token'], $token);
 }
 
+function normalizaCampos($dados, $campo) {
+    
+    $resultado = trim($dados[$campo] ?? null);
+
+    if ($campo == 'email'){
+        $resultado = mb_strtolower($resultado);
+    }
+
+    if (str_contains($campo, 'nome') || str_contains($campo, 'pastor') || str_contains($campo, 'igreja') || $campo == 'profissao' || $campo == 'logradouro' || $campo == 'complemento' || $campo == 'cidade' ) {
+        $resultado = mb_strtoupper($resultado);
+    }
+
+    return $resultado;
+}
+
 //Função para validação de campos
 function validaCampos($dados, $regras) {
     $erros = [];
+
     foreach ($regras as $campo => $regra) {
+        $dados[$campo] = normalizaCampos($dados, $campo);
+
         if ($regra['required'] && empty($dados[$campo])) {
             $erros[] = "O campo {$campo} é obrigatório.";
+            continue;
         }
+
         if (isset($regra['max']) && strlen($dados[$campo]) > $regra['max']) {
             $erros[] = "O campo {$campo} excede o tamanho máximo ({$regra['max']} caracteres).";
         }
+
+        if (isset($regra['format']) && !empty($dados[$campo]) && !preg_match($regra['format'], $dados[$campo])) {
+            $erros[] = "O campo {$campo} está incompleto ou com formato inválido.";
+            continue;
+        }
+
+        if (isset($regra['select']) && !in_array($dados[$campo], $regra['select'], true)) {
+            $erros[] = "Valor inválido para o campo {$campo}.";
+            continue;
+        }
+
+        if (isset($regra['email']) && !empty($dados[$campo]) && !filter_var($dados[$campo], $regra['email'])) {
+            $erros[] = "O campo e-mail está incompleto ou com formato inválido";
+        }
     }
+
     return $erros;
 }
 
@@ -38,8 +73,7 @@ function addMembro($dados) {
     $conn = null;
 
     try {
-        $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $pass,
-        [
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;", $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false
@@ -49,7 +83,7 @@ function addMembro($dados) {
         $queryProf = "INSERT INTO profissaos (nome, church_id, user_id, created, modified) VALUES (:nome, 22, 66, NOW(), NOW())";
 
         $stmtProf = $conn->prepare($queryProf);
-        $stmtProf->bindValue(':nome', trim(mb_strtoupper($dados['profissao'])));
+        $stmtProf->bindValue(':nome', $dados['profissao']);
         $stmtProf->execute();
 
         $profissao_id = $conn->lastInsertId();
@@ -66,30 +100,30 @@ function addMembro($dados) {
         $dataprofe    = validaData($dados['dataprofe'] ?? null);
 
         $stmtMemb = $conn->prepare($queryMemb);
-        $stmtMemb->bindValue(':nome', trim(mb_strtoupper($dados['nome'])));
+        $stmtMemb->bindValue(':nome', $dados['nome']);
         $stmtMemb->bindValue(':sexo', $dados['sexo']);
         $stmtMemb->bindValue(':datanascimento', $dados['datanascimento']);
-        $stmtMemb->bindValue(':naturalidade', trim(mb_strtoupper($dados['naturalidade'])));
-        $stmtMemb->bindValue(':email', trim(mb_strtolower($dados['email'])));
+        $stmtMemb->bindValue(':naturalidade', $dados['naturalidade']);
+        $stmtMemb->bindValue(':email', $dados['email']);
         $stmtMemb->bindValue(':cel', $dados['cel']);
         $stmtMemb->bindValue(':fone', $dados['fone']);
         $stmtMemb->bindValue(':rg', $dados['rg']);
         $stmtMemb->bindValue(':cpf', $dados['cpf']);
         $stmtMemb->bindValue(':estadocivil', $dados['estadocivil']);
         $stmtMemb->bindValue(':datacasamento', $datacasamento);
-        $stmtMemb->bindValue(':nomeconjuge', trim(mb_strtoupper($dados['nomeconjuge'])));
+        $stmtMemb->bindValue(':nomeconjuge', $dados['nomeconjuge']);
         $stmtMemb->bindValue(':escolaridade_id', $dados['escolaridade']);
         $stmtMemb->bindValue(':profissao_id', $profissao_id);
         $stmtMemb->bindValue(':batizado', $dados['batizado']);
         $stmtMemb->bindValue(':databatismo', $databatismo);
-        $stmtMemb->bindValue(':pastorbatismo', trim(mb_strtoupper($dados['pastorbatismo'])));
-        $stmtMemb->bindValue(':igrejabatismo', trim(mb_strtoupper($dados['igrejabatismo'])));
+        $stmtMemb->bindValue(':pastorbatismo', $dados['pastorbatismo']);
+        $stmtMemb->bindValue(':igrejabatismo', $dados['igrejabatismo']);
         $stmtMemb->bindValue(':profissaofe', $dados['profissaofe']);
         $stmtMemb->bindValue(':dataprofe', $dataprofe);
-        $stmtMemb->bindValue(':pastorprofe', trim(mb_strtoupper($dados['pastorprofe'])));
-        $stmtMemb->bindValue(':igrejaprofe', trim(mb_strtoupper($dados['igrejaprofe'])));
-        $stmtMemb->bindValue(':nomemae', trim(mb_strtoupper($dados['nomemae'])));
-        $stmtMemb->bindValue(':nomepai', trim(mb_strtoupper($dados['nomepai'])));
+        $stmtMemb->bindValue(':pastorprofe', $dados['pastorprofe']);
+        $stmtMemb->bindValue(':igrejaprofe', $dados['igrejaprofe']);
+        $stmtMemb->bindValue(':nomemae', $dados['nomemae']);
+        $stmtMemb->bindValue(':nomepai', $dados['nomepai']);
         $stmtMemb->execute();
 
         $membro_id = $conn->lastInsertId();
@@ -98,12 +132,12 @@ function addMembro($dados) {
         VALUES (:logradouro, :numero, :complemento, :bairro, :cep, :cidade, :estado, :membro_id, 66, 22, NOW(), NOW())";
 
         $stmtEnd = $conn->prepare($queryEnd);
-        $stmtEnd->bindValue(':logradouro', trim(mb_strtoupper($dados['logradouro'])));
+        $stmtEnd->bindValue(':logradouro', $dados['logradouro']);
         $stmtEnd->bindValue(':numero', $dados['numero']);
-        $stmtEnd->bindValue(':complemento', trim(mb_strtoupper($dados['complemento'])));
+        $stmtEnd->bindValue(':complemento', $dados['complemento']);
         $stmtEnd->bindValue(':cep', $dados['cep']);
-        $stmtEnd->bindValue(':bairro', trim(mb_strtoupper($dados['bairro'])));
-        $stmtEnd->bindValue(':cidade', trim(mb_strtoupper($dados['cidade'])));
+        $stmtEnd->bindValue(':bairro', $dados['bairro']);
+        $stmtEnd->bindValue(':cidade', $dados['cidade']);
         $stmtEnd->bindValue(':estado', $dados['estado']);
         $stmtEnd->bindValue(':membro_id', $membro_id);
         $stmtEnd->execute();
@@ -112,9 +146,12 @@ function addMembro($dados) {
         return ['status' => true, 'msg' => "Dados enviados com sucesso."];
     
     } catch (PDOException $err) {
-        $conn->rollBack();
-        error_log($err->getMessage());
 
+        if ($conn instanceof PDO && $conn->inTransaction()) {
+            $conn->rollBack();
+        }
+    
+        error_log($err->getMessage());
         if (isset($err->errorInfo[1]) && $err->errorInfo[1] == 1062) {
             return ['status' => false, 'msg' => 'CPF já cadastrado.'];
         }
@@ -124,40 +161,43 @@ function addMembro($dados) {
     }
 }
 
+$data_format='/^\d{4}-\d{2}-\d{2}$/';
+$select_format=['1', '2'];
+
 $regras = [
     'nome' => ['required' => true, 'max' => 100],
-    'sexo' => ['required' => true],
-    'datanascimento' => ['required' => true],
+    'sexo' => ['required' => true, 'select' => $select_format],
+    'datanascimento' => ['required' => true, 'format' => $data_format],
     'naturalidade' => ['required' => true],
     
     'logradouro' => ['required' => true, 'max' => 50],
     'numero' => ['required' => true],
     'bairro' => ['required' => true, 'max' => 30],
-    'cep' => ['required' => true, 'max' => 9],
+    'cep' => ['required' => true, 'max' => 9, 'format' => '/^\d{5}\-\d{3}$/'],
     'cidade' => ['required' => true],
     'estado' => ['required' => true, 'max' => 2],
     
-    'email' => ['required' => false],
+    'email' => ['required' => false, 'email' => FILTER_VALIDATE_EMAIL],
     'cel' => ['required' => false],
     'fone' => ['required' => false],
 
-    'rg' => ['required' => true, 'max' => 12],
-    'cpf' => ['required' => true, 'max' => 14],
-    'estadocivil' => ['required' => true],
-    'datacasamento' => ['required' => false],
+    'rg' => ['required' => true, 'max' => 12, 'format' => '/^\d{2}\.\d{3}\.\d{3}-[Xx\d]$/'],
+    'cpf' => ['required' => true, 'max' => 14, 'format' => '/^\d{3}\.\d{3}\.\d{3}-\d{2}$/'],
+    'estadocivil' => ['required' => true, 'select' => ['1', '2', '3', '4', '5', '6']],
+    'datacasamento' => ['required' => false, 'format' => '/^\d{4}-\d{2}-\d{2}$/'],
     'nomeconjuge' => ['required' => false],
     'nomemae' => ['required' => false],
     'nomepai' => ['required' => false],
-    'escolaridade' => ['required' => true],
+    'escolaridade' => ['required' => true, 'select' => ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']],
     'profissao' => ['required' => true, 'max' => 80],
     
-    'batizado' => ['required' => true],
-    'databatismo' => ['required' => false],
+    'batizado' => ['required' => true, 'select' => $select_format],
+    'databatismo' => ['required' => false, 'format' => $data_format],
     'pastorbatismo' => ['required' => false],
     'igrejabatismo' => ['required' => false],
     
-    'profissaofe' => ['required' => true],
-    'dataprofe' => ['required' => false],
+    'profissaofe' => ['required' => true, 'select' => $select_format],
+    'dataprofe' => ['required' => false, 'format' => $data_format],
     'pastorprofe' => ['required' => false],
     'igrejaprofe' => ['required' => false],
 ];
