73ff7d3bb4ec3e011bb0a4430566b92a5b348918
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-02-26 17:36:41 -0300

    atualiza-membros(fix): Correção de segurança (Web:S5725).

--- public/index.php
+++ public/index.php
@@ -18,7 +18,10 @@ if (empty($_SESSION['csrf_token'])) {
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link href="assets/css/appStyle.css" rel="stylesheet" />
     <link href="assets/img/logo-fundo-branco.png" rel="icon" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -445,7 +448,9 @@ if (empty($_SESSION['csrf_token'])) {
             <i class="fa-brands fa-instagram"></i> <a href="https://www.instagram.com/ipjovens/" target="_blank">ipjovens</a></span>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossorigin="anonymous"></script>
     <script type="text/javascript" src="assets/js/app.js"></script>
     <script type="text/javascript" src="assets/js/mask.js"></script>
     <script type="text/javascript" src="assets/js/toggleField.js"></script>
